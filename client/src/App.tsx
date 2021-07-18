5f2ff780cbb46e26ae1a75c6d3c00d78c32d9546
acyang97 <acyang97@gmail.com>
2021-07-19 00:31:08 +0800

    Created basic auth pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
+import store from "./store";
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>
-      <Router>
-        <Route exact path="/" component={Landing} />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <Provider store={store}>
+      <React.Fragment>
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Router>
+      </React.Fragment>
+    </Provider>
   );
 };
 
--- client/src/components/layout/Landing.tsx
+++ client/src/components/layout/Landing.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Landing = () => {
+const Landing: React.FC = () => {
   return <div>Landing Page</div>;
 };
 
